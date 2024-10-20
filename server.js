f32ca17589707cf3b04058c2eaf769e80e827e53
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-20 18:56:17 +0300

    q1

---
+++ server.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client connected!');
+  
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    // Відправляємо повідомлення всім клієнтам
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
